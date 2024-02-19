7cedd10a7bf86dac1b49df4e582b6d09466248ab
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-02-19 13:13:30 -0500

    model

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Christopher Peralta on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
